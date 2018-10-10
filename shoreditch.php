3db284d6c58e8825fa85443027438c645705b99b
Jon Goldberg <jon@megaphonetech.com>
2018-10-10 17:06:40 -0400

    temporary fix for Shoreditch QuickSearch to work on D8

--- shoreditch.php
+++ shoreditch.php
@@ -128,8 +128,8 @@ function shoreditch_civicrm_coreResourceList(&$items, $region) {
     CRM_Core_Resources::singleton()->addScriptFile('org.civicrm.shoreditch', 'base/js/tab.js', 1000, 'html-header');
     CRM_Core_Resources::singleton()->addScriptFile('org.civicrm.shoreditch', 'base/js/button.js', 1000, 'html-header');
     CRM_Core_Resources::singleton()->addScriptFile('org.civicrm.shoreditch', 'js/noConflict.js', 1001, 'html-header');
-    CRM_Core_Resources::singleton()->addScriptFile('org.civicrm.shoreditch', 'js/add-missing-date-addons.js');
-    CRM_Core_Resources::singleton()->addScriptFile('org.civicrm.shoreditch', 'js/main-menu.js');
+    CRM_Core_Resources::singleton()->addScriptFile('org.civicrm.shoreditch', 'js/add-missing-date-addons.js', 1002, 'html-header');
+    CRM_Core_Resources::singleton()->addScriptFile('org.civicrm.shoreditch', 'js/main-menu.js', '1003', 'html-header');
   }
 }
 
